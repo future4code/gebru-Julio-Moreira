011a12a8196f11817bace077aae037b2f5b4a843
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-11-11 22:13:47 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,53 @@
+
+/** Exercício 1 
+ *  false
+  false
+ true
+ boolean   */
+
+//  let primeiroNumero = prompt("Digite um numero!") 
+//  let segundoNumero = prompt("Digite outro numero!")
+ 
+//  const  primeiroNumero
+//  const  segundoNumero
+//  soma = 
+//   console.log(soma);
+
+
+
+//c) **Imprima na console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", seguido pela resposta (`true` ou `false`)
+
+//d) **Imprima na console** a diferença de idade (não tem problema se sair um número negativo)
+
+//Exercício 1
+// let idadeuser = prompt("Qual sua idade usuário!")
+//  let melhoramigo = prompt("Idade do seu melhor amigo!")
+
+//  console.log('Sua idade é maior do que a do seu melhor amigo?', idadeuser > melhoramigo);
+// console.log(idadeuser - melhoramigo);
+
+
+// //Exercício 2
+//let numeropar = prompt("Digite um número par!")
+ //console.log(numeropar % 2);
+// // o número par apresenta 0 e o ímpar 1
+
+//Exercício 3
+
+
+
+
+
+//Exercício 4
+
+// let n1 = prompt("Digite o primeiro número!")
+// let n2 = prompt("Digite o segundo número!")
+
+// console.log('O primeiro numero é maior que segundo?', n1 >= n2);
+// console.log('O primeiro numero é igual ao segundo?', n1 === n2);
+// console.log('O primeiro numero é divisível pelo segundo?', n1 % n2 === 0);
+// console.log('O segundo numero é divisível pelo primeiro?', n2 % n1 === 0);
+
+
+
+
