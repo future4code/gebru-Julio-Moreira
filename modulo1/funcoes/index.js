1c3f38b7af98cca664a3963c498c5c65bff068a6
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-11-19 20:14:10 -0300

    ainda não concluído a tarefa 3

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,96 @@
+//exercício de escrita
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// minhaFuncao(2),
+// minhaFuncao(10)
+//10,50
+// ao retirar o console não mostrou nada
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+// Essa função adiciona um texto em maiúsculo e  junto com a palavra cenoura
+// true, true, true 
+
+//Exercício 1
+//  function sobremim(){
+//     console.log("Eu sou Julio, tenho 33 anos, moro em São Paulo e sou estudante.");
+//  }
+//  sobremim()
+// let nome = "Julio"
+// let idade = 33
+// let cidade = "São Paulo"
+// let profissao = "estagiário"
+
+// function sobremim(nome, idade, cidade, profissão){
+//     return nome
+//     return  idade
+//     return  cidade
+//     return  profissao
+// }
+// console.log(sobremim(`Eu sou ${nome}, tenho  ${idade} anos, moro em  ${cidade} e sou  ${profissao}.`));
+
+//Exercício 2
+// function somarNumeros(num1, num2){
+//     const resultado = num1 + num2
+//     return resultado
+// }
+// somarNumeros()
+// console.log(somarNumeros(2,5));
+
+// function maiorOuigual(num1, num2){
+//     const resultado = num1 >= num2
+//     return resultado
+// }
+// maiorOuigual()
+// console.log(maiorOuigual(5,5));
+
+// function numParouImpar(num){
+//     const resultado = num%2 ==0
+//     return resultado
+// }
+// numParouImpar()
+// console.log(numParouImpar(4));
+
+// let texto1 = "oi meu amor"
+
+// function mensagem(texto)
+// {
+//     texto = texto.toUpperCase() 
+//     console.log(texto)
+//     texto = texto.length
+//     console.log(texto)
+// }
+// mensagem(texto1)
+
+// Exercício 3
+function verNumeros(num1, num2){
+         const soma = num1 + num2
+        
+         const diferenca = num1 % num2
+        
+         //const resultado = num1 * num2
+         
+        // const resultado = num1 / num2
+         
+         return soma;
+
+         return diferenca
+       
+     }
+     verNumeros()
+     
+
+let num1 = Number(prompt("Digite um numero"));
+let num2 = Number(prompt("Digite outro numero"));
+console.log(verNumeros(num1,num2));
+console.log(verNumeros(num1,num2));
+
+
