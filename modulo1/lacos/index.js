4bb4157ccd4523fbf99da26415304aa832b2fb32
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-11-30 19:28:19 -0300

    irei refazer o exercício 2

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,96 @@
+//Exercício 1
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+// //imprimiu 10
+// //o código está incrementando valor na variável i
+
+// Exercício 2
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+// // foi impresso 19, 21, 23,25,27,30
+
+// Exercício 3
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+// aparece 4 linhas e na quarta linha 4 asteriscos 
+
+// Exercício 1
+// const quantidadeTotal = Number(prompt("Quantos pets vc tem?"))
+// let quantidadeAtual = 0
+// if(quantidadeTotal === 0 ){
+//     console.log("Que pena você pode adotar um pet")
+// }else{
+//     let pets = "pets"
+    
+//     quantidadeAtual = Number(prompt("Digite o nome dos seus pets"))
+//     for(let pets = 0;pets > quantidadeAtual + 1; pets++){
+//         pets += ""
+
+// }
+// console.log(pets)
+//   quantidadeAtual++
+//       }
+
+// exercício 2
+// a
+// function arrayOriginal(){
+//     const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//     console.log(array);
+// }
+// b não  consegui fazer
+// arrayOriginal()
+
+// function arrayOriginal(arrayinicial){
+//     const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//     return array / 10
+// }
+// console.log(array)
+// arrayOriginal()
+
+//letra c
+
+//  function arrayOriginal(arrayinicial){
+//      const array = [80, 30, 130, 40, 60, 70, 120, 90, 110]
+//      console.log(array);
+//  }
+//  arrayOriginal()
+
+// letra e
+
+// let valorMaximo = 110
+// let valorMinimo = 21
+//   function arrayOriginal(){
+ 
+//      const array = [80, 30, 130, 40, 60, 70, 120, 90, 110]
+//      for(let i = 0; i< array.lenght; i++){
+//          const elemento1 = array[i]
+//          const elemento2 = array[i]
+//          if(elemento1 > valorMaximo && elemento2 === valorMinimo){
+//              valorMaximo = elemento1 
+//              valorMinimo = elemento2
+//          }
+//      }
+//      return valorMaximo, valorMinimo
+//  }
+//  arrayOriginal()
+
+  
+
+
